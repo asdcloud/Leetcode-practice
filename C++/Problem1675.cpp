947b82793e169111a54d3cb51f73dc1fed19ce00
asdcloud <asdcloud01@gmail.com>
2023-03-30 22:45:46 +0800

    go

---
+++ C++/Problem1675.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        int first_max = 0, second_max = 0, first_min = INT_MAX, second_min = INT_MAX;
+        for (int a = 0; a < nums.size(); a++) {
+            if (nums[a] > first_max) {
+                
+            }
+        }
+    }
+};
+
+int main() {
+
+}
